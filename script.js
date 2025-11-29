0c27bb065317c3be7d651079e8c1e7be2e0a926b
Rafla <soberarafal2004@gmail.com>
2025-11-29 18:46:11 +0100

    init: pierwsza wersja portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Portfolio uruchomione!");
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+sections.forEach(section => observer.observe(section));
